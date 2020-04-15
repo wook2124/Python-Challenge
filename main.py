6e521d20e40d600e54de5e0e8caef3445cb1f27f
Young <wook2124@naver.com>
2020-04-15 19:16:05 +0000

    DAY 3 OF 4 PYTHON CHALLENGE

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-def age_check(age):
-  print(f"You are {age}")
-  if age < 18:
-    print("You can't drink!")
-  elif age == 18 or 19:
-    print("You are new to this!")
-  elif age > 19 and age < 25:
-    print("You are still kind of young!")
-  else:
-    print("Enjoy your drink!")
+def plus(a, b, *args):
+  print(args)
+  return (a + b)
 
-
-age_check(19)
+plus(1, 3, 45, 3, 5)
