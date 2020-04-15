37a4abbbf2c25f6e6b394b252cb4ede985966cee
Young <wook2124@naver.com>
2020-04-15 15:39:49 +0000

    #1.11 if else and or

--- #1/#1.11 if else and or.py
+++ #1/#1.11 if else and or.py
@@ -1,3 +1,15 @@
+# if, else는 들여쓰기 주의
+def plus(a, b):
+  if type(a & b) is int or float :
+    return a + b
+  else:
+    return None
+
+print(plus(12, 10))
+
+
+# elif는 if의 또 다른 condition임
+# elif의 condition까지 만족시키지 못하면 else가 실행됨
 def age_check(age):
   print(f"You are {age}")
   if age < 18:
@@ -8,6 +20,5 @@ def age_check(age):
     print("You are still kind of young!")
   else:
     print("Enjoy your drink!")
-
 
 age_check(19)
