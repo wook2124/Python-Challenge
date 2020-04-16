95a1f09606a085c84dfafdabb6362ee41c6f7790
Young <wook2124@naver.com>
2020-04-16 08:58:47 +0000

    DAY 4 OF 4 PYTHON CHALLENGE

--- Assignment/DAY 4 OF 4 PYTHON CHALLENGE.py
+++ Assignment/DAY 4 OF 4 PYTHON CHALLENGE.py
@@ -3,24 +3,31 @@ import requests
 
 os.system("clear")
 clear = os.unlink
+y = "y"
+n = "n"
 
 print("Welcome to IsItDown.py!")
 print("Please write a URL or URLs you want to check. (separated by comma)")
 
 URL = input("")
+if ".com" not in input:
+  print("You have to put '.com' inside your answer!")
+else:
+  print(f"https://{URL} is up!")
+  qestion = input("Do you want to start over? y/n")
+
+while True:
+  if qestion == y:
+    clear()
+    continue
+  elif qestion == n:
+    print("See you on the Next Request!")
+    break
+  else:
+    print("That's invalid answer!")
+
 r = requests.get(URL)
 r.status_code == requests.codes.ok
 URL.strip()
 URL.split()
 # try, except
-
-qestion = input("Do you want to start over? y/n")
-qestion
-y = "y"
-n = "n"
-if qestion == y:
-  clear()
-elif qestion == n:
-  print("See you on the Next Request!")
-else:
-  print("That's invalid answer!")
