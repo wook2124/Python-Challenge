aea994256cbe1572c6ccc0523298f0c7b36b0693
Young <wook2124@naver.com>
2020-05-30 07:06:53 +0000

    DAY 6 OF 14 PYTHON CHALLENGE

--- Assignment 2/DAY 6 OF 14 PYTHON CHALLENGE.py
+++ Assignment 2/DAY 6 OF 14 PYTHON CHALLENGE.py
@@ -76,18 +76,22 @@ target_country = ask_country("\n\nNow choose another country.")
 amount = ask_amount(user_country, target_country)
 
 
+# 환전 사이트
 currency_url = "https://transferwise.com/gb/currency-converter/"
 
+# code 값
 user_code = user_country['code']
 target_code = target_country['code']
 
 # KRW => USD 10000원 환전 url = {https://transferwise.com/gb/currency-converter/}{krw}-to-{usd}-rate?amount={10000}
 currency_request = requests.get(f"{currency_url}{user_code}-to-{target_code}-rate?amount={amount}")
 currency_soup = BeautifulSoup(currency_request.text, "html.parser")
-
+# 환전한 amount를 보여주는 input란을 가지고옴
 currency_result = currency_soup.find("input", {"id": "cc-amount-to"})
 if currency_result:
   currency_result = currency_result['value']
+  # 처음에 입력한 amount
   amount = format_currency(amount, user_code, locale="ko_KR")
+  # 환전해서 나온 amount
   currency_result = format_currency(currency_result, target_code, locale="ko_KR")
   print(f"{amount} is {currency_result}")
